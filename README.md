056a0158eec68aa6b61f96c227ae09a2547ba8c5
TimotheeGimbert <timothee.gimbert@free.fr>
2022-01-24 15:33:23 +0100

    wreate webpack config and subfolders

---
+++ README.md
@@ -0,0 +1,23 @@
+1) At the root, create index.html and a directory /src containing index.js (/src/style, /src/assets, /src/assets/images, /src/assets/fonts, etc ...)
+
+2) Inside index.html, place : 
+<link rel="stylesheet" href="./dist/bundle.css" /> <!-- In <head> --> 
+<script src="./dist/bundle.js"></script> <!-- Bottom of <body> --> 
+
+3) Generate package.json with : 
+npm init -y
+
+4) Install webpack and its CLI for development (devDependencies) with : 
+npm i --save-dev webpack webpack-cli
+
+5) At the root, create webpack.config.js containing :
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '',
+  },
+  mode: 'development',
+};
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// Webpack utilise ce module Node.js pour travailler avec les dossiers.
+const path = require('path');
+
+// Ceci est la configuration principale de ton projet.
+// Ici, tu peux écrire les différentes options que tu souhaites, et dire à Webpack quoi faire.
+module.exports = {
+
+  // Ceci est le chemin vers le "point d'entrée" de ton app.
+  // C'est depuis ce fichier que Webpack commencera à travailler.
+  entry: './src/index.js',
+
+  // C'est ici qu'on dit à Webpack où mettre le fichier résultant avec tout ton JS.
+  output: {
+    // Le chemin relatif au dossier courant (la racine du projet)
+    path: path.resolve(__dirname, 'dist'),
+    // Le nom du fichier de ton bundle JS
+    filename: 'bundle.js',
+    // L'URL relatif au HTML pour accéder aux assets de l'application. Ici,
+    // le HTML est situé à la racine du projet, donc on met une chaîne vide.
+    publicPath: '',
+  },
+
+  // Par défaut, le mode de Webpack est "production". En fonction de ce qui est
+  // écrit ici, tu pourras appliquer différentes méthodes dans ton bundle final.
+  // Pour le moment, nous avons besoin du mode "développement", car nous n'avons,
+  // par exemple, pas besoin de minifier notre code.
+  mode: 'development',
+};
